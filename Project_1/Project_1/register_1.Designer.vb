6c1d60f8a6b0ca9525ab8e0a5cb8c33e410ac8f4
Dhruv <dhruvprajapati66572@gmail.com>
2022-04-15 23:11:34 +0530

    BackGround Changed

--- Project_1/Project_1/register_1.Designer.vb
+++ Project_1/Project_1/register_1.Designer.vb
@@ -23,32 +23,32 @@ Partial Class register_1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.TextBox9 = New System.Windows.Forms.TextBox()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.TextBox10 = New System.Windows.Forms.TextBox()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.TextBox12 = New System.Windows.Forms.TextBox()
         Me.Label13 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -86,101 +86,102 @@ Partial Class register_1
         Me.Panel1.Size = New System.Drawing.Size(609, 558)
         Me.Panel1.TabIndex = 0
         '
-        'Label1
+        'Button1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(298, 35)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(176, 46)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Register"
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(236, 487)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(162, 41)
+        Me.Button1.TabIndex = 31
+        Me.Button1.Text = "Register"
+        Me.Button1.UseVisualStyleBackColor = True
         '
-        'Label2
+        'MaskedTextBox4
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(32, 30)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(83, 16)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "First Name"
+        Me.MaskedTextBox4.BeepOnError = True
+        Me.MaskedTextBox4.Location = New System.Drawing.Point(35, 384)
+        Me.MaskedTextBox4.Mask = "000-000"
+        Me.MaskedTextBox4.Name = "MaskedTextBox4"
+        Me.MaskedTextBox4.Size = New System.Drawing.Size(80, 22)
+        Me.MaskedTextBox4.TabIndex = 30
         '
-        'TextBox2
+        'MaskedTextBox3
         '
-        Me.TextBox2.Location = New System.Drawing.Point(325, 49)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(231, 22)
-        Me.TextBox2.TabIndex = 3
+        Me.MaskedTextBox3.BeepOnError = True
+        Me.MaskedTextBox3.Location = New System.Drawing.Point(325, 235)
+        Me.MaskedTextBox3.Mask = "LLLLLL000"
+        Me.MaskedTextBox3.Name = "MaskedTextBox3"
+        Me.MaskedTextBox3.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.MaskedTextBox3.Size = New System.Drawing.Size(231, 22)
+        Me.MaskedTextBox3.TabIndex = 29
         '
-        'Label3
+        'MaskedTextBox2
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(322, 30)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(82, 16)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Last Name"
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(35, 235)
+        Me.MaskedTextBox2.Mask = "LLLLLL000"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(226, 22)
+        Me.MaskedTextBox2.TabIndex = 28
         '
-        'Label4
+        'MaskedTextBox1
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(32, 91)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(47, 16)
-        Me.Label4.TabIndex = 4
-        Me.Label4.Text = "Email"
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(325, 172)
+        Me.MaskedTextBox1.Mask = "  /  /    "
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(47, 22)
+        Me.MaskedTextBox1.TabIndex = 27
         '
-        'TextBox4
+        'ComboBox1
         '
-        Me.TextBox4.Location = New System.Drawing.Point(325, 110)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(231, 22)
-        Me.TextBox4.TabIndex = 9
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Male", "Female"})
+        Me.ComboBox1.Location = New System.Drawing.Point(35, 172)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(108, 24)
+        Me.ComboBox1.TabIndex = 26
         '
-        'Label5
+        'TextBox12
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(322, 91)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(103, 16)
-        Me.Label5.TabIndex = 8
-        Me.Label5.Text = "Account Type"
+        Me.TextBox12.Location = New System.Drawing.Point(325, 384)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(231, 22)
+        Me.TextBox12.TabIndex = 25
         '
-        'Label6
+        'Label13
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(32, 153)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(59, 16)
-        Me.Label6.TabIndex = 10
-        Me.Label6.Text = "Gender"
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(322, 365)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(60, 16)
+        Me.Label13.TabIndex = 24
+        Me.Label13.Text = "Country"
         '
-        'Label7
+        'Label12
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(322, 153)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(76, 16)
-        Me.Label7.TabIndex = 12
-        Me.Label7.Text = "Birth Date"
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(32, 365)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(97, 16)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = "Pascal Code"
         '
-        'Label8
+        'TextBox10
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(32, 216)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(76, 16)
-        Me.Label8.TabIndex = 14
-        Me.Label8.Text = "Password"
+        Me.TextBox10.Location = New System.Drawing.Point(325, 297)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(231, 22)
+        Me.TextBox10.TabIndex = 21
         '
-        'Label9
+        'Label11
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(322, 216)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(166, 16)
-        Me.Label9.TabIndex = 16
-        Me.Label9.Text = "Password Confirmation"
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(322, 278)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(34, 16)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "City"
         '
         'TextBox9
         '
@@ -199,53 +200,57 @@ Partial Class register_1
         Me.Label10.TabIndex = 18
         Me.Label10.Text = "Street Address"
         '
-        'TextBox10
+        'Label9
         '
-        Me.TextBox10.Location = New System.Drawing.Point(325, 297)
-        Me.TextBox10.Name = "TextBox10"
-        Me.TextBox10.Size = New System.Drawing.Size(231, 22)
-        Me.TextBox10.TabIndex = 21
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(322, 216)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(166, 16)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Password Confirmation"
         '
-        'Label11
+        'Label8
         '
-        Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(322, 278)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(34, 16)
-        Me.Label11.TabIndex = 20
-        Me.Label11.Text = "City"
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(32, 216)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(76, 16)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Password"
         '
-        'Label12
+        'Label7
         '
-        Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(32, 365)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(97, 16)
-        Me.Label12.TabIndex = 22
-        Me.Label12.Text = "Pascal Code"
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(322, 153)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(76, 16)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Birth Date"
         '
-        'TextBox12
+        'Label6
         '
-        Me.TextBox12.Location = New System.Drawing.Point(325, 384)
-        Me.TextBox12.Name = "TextBox12"
-        Me.TextBox12.Size = New System.Drawing.Size(231, 22)
-        Me.TextBox12.TabIndex = 25
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 153)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 16)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Gender"
         '
-        'Label13
+        'TextBox4
         '
-        Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(322, 365)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(60, 16)
-        Me.Label13.TabIndex = 24
-        Me.Label13.Text = "Country"
+        Me.TextBox4.Location = New System.Drawing.Point(325, 110)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(231, 22)
+        Me.TextBox4.TabIndex = 9
         '
-        'TextBox1
+        'Label5
         '
-        Me.TextBox1.Location = New System.Drawing.Point(35, 49)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(217, 22)
-        Me.TextBox1.TabIndex = 1
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(322, 91)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Account Type"
         '
         'TextBox3
         '
@@ -254,66 +259,62 @@ Partial Class register_1
         Me.TextBox3.Size = New System.Drawing.Size(217, 22)
         Me.TextBox3.TabIndex = 5
         '
-        'ComboBox1
+        'Label4
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Male", "Female"})
-        Me.ComboBox1.Location = New System.Drawing.Point(35, 172)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(108, 24)
-        Me.ComboBox1.TabIndex = 26
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(32, 91)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 16)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Email"
         '
-        'MaskedTextBox1
+        'TextBox2
         '
-        Me.MaskedTextBox1.Location = New System.Drawing.Point(325, 172)
-        Me.MaskedTextBox1.Mask = "  /  /    "
-        Me.MaskedTextBox1.Name = "MaskedTextBox1"
-        Me.MaskedTextBox1.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.MaskedTextBox1.Size = New System.Drawing.Size(47, 22)
-        Me.MaskedTextBox1.TabIndex = 27
+        Me.TextBox2.Location = New System.Drawing.Point(325, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(231, 22)
+        Me.TextBox2.TabIndex = 3
         '
-        'MaskedTextBox2
+        'Label3
         '
-        Me.MaskedTextBox2.Location = New System.Drawing.Point(35, 235)
-        Me.MaskedTextBox2.Mask = "LLLLLL000"
-        Me.MaskedTextBox2.Name = "MaskedTextBox2"
-        Me.MaskedTextBox2.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.MaskedTextBox2.Size = New System.Drawing.Size(226, 22)
-        Me.MaskedTextBox2.TabIndex = 28
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(322, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Last Name"
         '
-        'MaskedTextBox3
+        'TextBox1
         '
-        Me.MaskedTextBox3.BeepOnError = True
-        Me.MaskedTextBox3.Location = New System.Drawing.Point(325, 235)
-        Me.MaskedTextBox3.Mask = "LLLLLL000"
-        Me.MaskedTextBox3.Name = "MaskedTextBox3"
-        Me.MaskedTextBox3.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.MaskedTextBox3.Size = New System.Drawing.Size(231, 22)
-        Me.MaskedTextBox3.TabIndex = 29
+        Me.TextBox1.Location = New System.Drawing.Point(35, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(217, 22)
+        Me.TextBox1.TabIndex = 1
         '
-        'MaskedTextBox4
+        'Label2
         '
-        Me.MaskedTextBox4.BeepOnError = True
-        Me.MaskedTextBox4.Location = New System.Drawing.Point(35, 384)
-        Me.MaskedTextBox4.Mask = "000-000"
-        Me.MaskedTextBox4.Name = "MaskedTextBox4"
-        Me.MaskedTextBox4.Size = New System.Drawing.Size(80, 22)
-        Me.MaskedTextBox4.TabIndex = 30
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 16)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "First Name"
         '
-        'Button1
+        'Label1
         '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(236, 487)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(162, 41)
-        Me.Button1.TabIndex = 31
-        Me.Button1.Text = "Register"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(298, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 46)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Register"
         '
         'register_1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
         Me.ClientSize = New System.Drawing.Size(768, 693)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel1)
