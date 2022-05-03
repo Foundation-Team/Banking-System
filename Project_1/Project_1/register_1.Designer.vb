e122fe8dde5185cdd86d7d400a6440bc0a9a5c02
H.$ <het.solanki109@gmail.com>
2022-05-03 19:14:25 +0530

    Color Changed

--- Project_1/Project_1/register_1.Designer.vb
+++ Project_1/Project_1/register_1.Designer.vb
@@ -332,6 +332,7 @@ Partial Class register_1
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Violet
         Me.Label1.Font = New System.Drawing.Font("Meiryo UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(408, 9)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
