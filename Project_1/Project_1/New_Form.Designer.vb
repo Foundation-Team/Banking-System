6876228e31ebfad641e33f34ec7c1351c81de0f2
H.$ <het.solanki109@gmail.com>
2022-05-03 19:47:53 +0530

    MenuStip Added

--- Project_1/Project_1/New_Form.Designer.vb
+++ Project_1/Project_1/New_Form.Designer.vb
@@ -22,18 +22,69 @@ Partial Class New_Form
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HomeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutUsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContactUsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HomeToolStripMenuItem, Me.LogToolStripMenuItem, Me.AboutUsToolStripMenuItem, Me.ContactUsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1135, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HomeToolStripMenuItem
+        '
+        Me.HomeToolStripMenuItem.Name = "HomeToolStripMenuItem"
+        Me.HomeToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
+        Me.HomeToolStripMenuItem.Text = "Home"
+        '
+        'LogToolStripMenuItem
+        '
+        Me.LogToolStripMenuItem.Name = "LogToolStripMenuItem"
+        Me.LogToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.LogToolStripMenuItem.Text = "Log"
+        '
+        'AboutUsToolStripMenuItem
+        '
+        Me.AboutUsToolStripMenuItem.Name = "AboutUsToolStripMenuItem"
+        Me.AboutUsToolStripMenuItem.Size = New System.Drawing.Size(84, 24)
+        Me.AboutUsToolStripMenuItem.Text = "About-Us"
+        '
+        'ContactUsToolStripMenuItem
+        '
+        Me.ContactUsToolStripMenuItem.Name = "ContactUsToolStripMenuItem"
+        Me.ContactUsToolStripMenuItem.Size = New System.Drawing.Size(94, 24)
+        Me.ContactUsToolStripMenuItem.Text = "Contact_Us"
         '
         'New_Form
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Coral
-        Me.ClientSize = New System.Drawing.Size(752, 583)
+        Me.ClientSize = New System.Drawing.Size(1135, 726)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "New_Form"
         Me.Text = "New_Form"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents HomeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutUsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContactUsToolStripMenuItem As ToolStripMenuItem
 End Class
